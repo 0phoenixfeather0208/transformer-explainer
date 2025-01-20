afd2ac57d6bfc26a9c3b5fbe132cd7f83f9d07a0
P.F. <111067437+0phoenixfeather0208@users.noreply.github.com>
2025-01-20 01:22:10 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		fs: {
-			// Allow serving files from one level up to the project root
-			allow: ['..']
-		}
-	}
+  plugins: [sveltekit()],
+  build: {
+    outDir: 'dist', // Set the output directory to 'dist'
+  },
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['..']
+    }
+  }
 });
